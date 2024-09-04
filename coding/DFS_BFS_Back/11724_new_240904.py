fa27b7dcdaa179e18708e65ba3a67958dd93987d
applzz <hws0602@naver.com>
2024-09-04 12:52:19 +0900

    11724 resolve success

--- coding/DFS_BFS_Back/11724_new_240904.py
+++ coding/DFS_BFS_Back/11724_new_240904.py
@@ -1,18 +1,37 @@
-N,M = map(int, input().split(' '))
+from sys import stdin
+import sys
 
-checkList = [0 for i in range(N)]
+sys.setrecursionlimit(10**6)
+
+N,M = map(int, stdin.readline().split(' '))
+
+graph = [[0] * N for j in range(N)]
+checkList = [0]* N
 count = 0
 
+# 그래프 작성
 for i in range(M):
-    a, b = map(int, input().split(' '))
+    a, b = map(int, stdin.readline().split(' '))
+    a = a -1
+    b = b -1
+    graph[a][b] = 1
+    graph[b][a] = 1
 
-    if checkList[a - 1]:
-        checkList[b - 1] = 1
-    else:
-        checkList[a - 1] = 1
-        checkList[b - 1] = 1
+
+# 노드 체크
+def check_node(index):
+    # print(f'index : {index}')
+    for j in range(N):
+        if graph[index][j] == 1 and checkList[j] == 0:
+            checkList[j] = 1
+            check_node(j)
+
+
+# 노드가 어디로 가는지 체크
+for i in range(N):
+    if checkList[i] == 0:
         count += 1
+        check_node(i)
 
 print(count)
-
 
