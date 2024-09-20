20bf4f9c5fa9e64ffb876fc35c6912ca75c37a4b
seongho220905 <seonhgo.lim@diquest.com>
2024-09-20 18:10:20 +0900

    12851

--- coding/DFS_BFS_Back/12851.py
+++ coding/DFS_BFS_Back/12851.py
@@ -15,30 +15,34 @@ while len(que) > 0:
 
     # 현 위치가 K 인 값이 1개 이상 존재하고, 현재 위치의 시간이 최단 시간보다 길 때, 반복문 종료
     if correct_cnt > 0 and now_sts[1] > correct_position_cnt:
-        # print(f'now_position : {now_position}')
         print(correct_position_cnt)
         print(correct_cnt)
         break
 
     # 현 위치가 K 가 되었을 때, correct_cnt 추가
     if now_sts[0] == K:
-        # print(f'now_position : {now_sts}')
         correct_position_cnt =  now_sts[1]
         correct_cnt = correct_cnt + 1
     else:
         # 현재 위치
-        if 0<= now_sts[0] - 1 <= 100000 and prev_positions[now_sts[0] - 1] == 0:
+        if 0 <= now_sts[0] - 1 <= 100000 and prev_positions[now_sts[0] - 1] == 0:
             # 갈 수 있는 위치면, que 에 추가
             que.append((now_sts[0] - 1, now_sts[1] + 1))
+
             # 지나간 경로에 추가
             if now_sts[0] - 1 != K:
                 prev_positions[now_sts[0] - 1] = 1
-
-        if 0<= now_sts[0] + 1 <= 100000 and prev_positions[now_sts[0] + 1] == 0:
+        if 0 <= now_sts[0] + 1 <= 100000 and prev_positions[now_sts[0] + 1] == 0:
+            # 갈 수 있는 위치면, que 에 추가
             que.append((now_sts[0] + 1, now_sts[1] + 1))
+
+            # 지나간 경로에 추가
             if now_sts[0] + 1 != K:
                 prev_positions[now_sts[0] + 1] = 1
-        if 0<= now_sts[0] * 2 <= 100000 and prev_positions[now_sts[0] *2] == 0:
+        if 0 <= now_sts[0] * 2 <= 100000 and prev_positions[now_sts[0] *2] == 0:
+            # 갈 수 있는 위치면, que 에 추가
             que.append((now_sts[0] * 2, now_sts[1] + 1))
-            if now_sts[0] *2 != K:
+
+            # 지나간 경로에 추가
+            if now_sts[0] * 2 != K:
                 prev_positions[now_sts[0] * 2] = 1
