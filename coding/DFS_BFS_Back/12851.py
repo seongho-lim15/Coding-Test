e9613e0d5d428baf7f2032f15713408b1a876640
seongho220905 <seonhgo.lim@diquest.com>
2024-09-20 17:58:57 +0900

    12851

--- coding/DFS_BFS_Back/12851.py
+++ coding/DFS_BFS_Back/12851.py
@@ -4,7 +4,7 @@ N, K = map(int, input().split(' '))
 que = deque()
 prev_positions = [0] * 100001
 
-que.append((N, 0, N))
+que.append((N, 0))
 prev_positions[N] = 1
 correct_cnt = 0
 correct_position_cnt = 0
@@ -27,19 +27,18 @@ while len(que) > 0:
         correct_cnt = correct_cnt + 1
     else:
         # 현재 위치
-        now_pstn = now_sts[0]
-        if 0<= now_pstn - 1 <= 100000 and prev_positions[now_pstn - 1] == 0:
+        if 0<= now_sts[0] - 1 <= 100000 and prev_positions[now_sts[0] - 1] == 0:
             # 갈 수 있는 위치면, que 에 추가
-            que.append((now_sts[0] - 1, now_sts[1] + 1, *now_sts[2:], now_sts[0] - 1))
+            que.append((now_sts[0] - 1, now_sts[1] + 1))
             # 지나간 경로에 추가
-            if now_pstn - 1 != K:
-                prev_positions[now_pstn - 1] = 1
+            if now_sts[0] - 1 != K:
+                prev_positions[now_sts[0] - 1] = 1
 
-        if 0<= now_pstn + 1 <= 100000 and prev_positions[now_pstn + 1] == 0:
-            que.append((now_sts[0] + 1, now_sts[1] + 1, *now_sts[2:], now_sts[0] + 1))
-            if now_pstn + 1 != K:
-                prev_positions[now_pstn + 1] = 1
-        if 0<= now_pstn * 2 <= 100000 and prev_positions[now_pstn *2] == 0:
-            que.append((now_sts[0] * 2, now_sts[1] + 1, *now_sts[2:], now_sts[0] * 2))
-            if now_pstn *2 != K:
-                prev_positions[now_pstn * 2] = 1
+        if 0<= now_sts[0] + 1 <= 100000 and prev_positions[now_sts[0] + 1] == 0:
+            que.append((now_sts[0] + 1, now_sts[1] + 1))
+            if now_sts[0] + 1 != K:
+                prev_positions[now_sts[0] + 1] = 1
+        if 0<= now_sts[0] * 2 <= 100000 and prev_positions[now_sts[0] *2] == 0:
+            que.append((now_sts[0] * 2, now_sts[1] + 1))
+            if now_sts[0] *2 != K:
+                prev_positions[now_sts[0] * 2] = 1
