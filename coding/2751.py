c99b9ffee4ccb908a59a72de6641cc9e6a9bd785
applzz <hws0602@naver.com>
2024-10-11 20:30:00 +0900

    11650

--- coding/2751.py
+++ coding/2751.py
@@ -2,6 +2,19 @@ import sys
 N = int(sys.stdin.readline())
 num_list = []
 
+# 비겁 정렬
+def normalSort():
+    for i in range(N):
+        input_num = int(sys.stdin.readline())
+        num_list.append(input_num)
+
+    num_list.sort()
+
+    for num in num_list:
+        print(num)
+
+normalSort()
+
 # 삽입 정렬
 def insertSort():
     for i in range(N):
@@ -23,25 +36,22 @@ def insertSort():
     for num in num_list:
         print(num)
 
-# 이진 삽입 정렬
-def biInsertSort():
-    def biInsert(idx):
-
-    for i in range(N):
-        input_num = int(sys.stdin.readline())
-
-        if i == 0:
-            num_list.append(input_num)
-        else:
-            num_mid_idx = len(num_list)//2
-
-            # num_list 의 중간 값이 들어온 값보다 작을 경우
-            if num_list[num_mid_idx] < input_num:
-
-            elif num_list[num_mid_idx] > input_num: # num_list 의 중간 값이 들어온 값보다 작을 경우
-
-
-
-
-    for num in num_list:
-        print(num)
+# # 이진 삽입 정렬
+# def biInsertSort():
+#     def biInsert(idx):
+#
+#     for i in range(N):
+#         input_num = int(sys.stdin.readline())
+#
+#         if i == 0:
+#             num_list.append(input_num)
+#         else:
+#             num_mid_idx = len(num_list)//2
+#
+#             # num_list 의 중간 값이 들어온 값보다 작을 경우
+#             if num_list[num_mid_idx] < input_num:
+#
+#             elif num_list[num_mid_idx] > input_num: # num_list 의 중간 값이 들어온 값보다 작을 경우
+#
+#     for num in num_list:
+#         print(num)
