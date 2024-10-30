7bdb8f0e83fb35fcc03db70f3bc5bb7907218313
applzz <hws0602@naver.com>
2024-10-30 19:57:34 +0900

    1012 해결

--- coding/11659.py
+++ coding/11659.py
@@ -1,17 +1,23 @@
 import sys
 
-N, M = map(int, sys.stdin.readline().strip().split(' '))
-num_arr = list(map(int, sys.stdin.readline().strip().split(' ')))
-memo_sum = [0] * N
 
-for i in range(N):
-    memo_sum[i] = memo_sum[i-1] + num_arr[i]
 
-for _ in range(M):
-    a, b = map(int, sys.stdin.readline().strip().split(' '))
-    a -=1
-    b -=1
-    if a == 0:
-        print(memo_sum[b])
-    else:
-        print(memo_sum[b] - memo_sum[a-1])
+v = [0,1,2,3]
+
+
+
+# N, M = map(int, sys.stdin.readline().strip().split(' '))
+# num_arr = list(map(int, sys.stdin.readline().strip().split(' ')))
+# memo_sum = [0] * N
+#
+# for i in range(N):
+#     memo_sum[i] = memo_sum[i-1] + num_arr[i]
+#
+# for _ in range(M):
+#     a, b = map(int, sys.stdin.readline().strip().split(' '))
+#     a -=1
+#     b -=1
+#     if a == 0:
+#         print(memo_sum[b])
+#     else:
+#         print(memo_sum[b] - memo_sum[a-1])
